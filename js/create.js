df4edc4b096dbd4e1f97ed869e3f17239c870537
Callum <callumjrobinson2020@gmail.com>
2022-04-28 16:50:20 +0100

    added functionality to switch between the weapon and character forms

---
+++ js/create.js
@@ -0,0 +1,26 @@
+(function() {
+    const requestSelector = document.querySelector('#method');
+    const dataTable = document.querySelector('#data-table');
+    const dataForm = document.querySelector('#data-form');
+    const characterForm = document.querySelector('#character-info');
+    const weaponForm = document.querySelector('#weapon-info');
+
+    function toggleForm(formWanted) {
+        if (formWanted == "Character") {
+            if (characterForm.classList.contains('hide')) characterForm.classList.remove('hide');
+            if (!weaponForm.classList.contains('hide')) weaponForm.classList.add('hide');
+        } else if (formWanted == "Weapon") {
+            if (!characterForm.classList.contains('hide')) characterForm.classList.add('hide');
+            if (weaponForm.classList.contains('hide')) weaponForm.classList.remove('hide');
+        }
+    }
+
+    requestSelector.addEventListener('change', function(event) {
+        if (this.value == 'Character') {
+            toggleForm("Character");
+        }
+        else if (this.value == 'Weapon') {
+            toggleForm("Weapon")
+        }
+    });
+})();
